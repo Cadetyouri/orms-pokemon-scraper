43873563bcad640224cb862dd4ed68b264a2fe8e
Cadetyouri <cadet.jesusyouri@gmail.com>
2020-12-18 04:08:05 -0500

    commit tree 45e450585caaa70cdb555b4c1892d68b866216d6

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
 class Pokemon
+    attr_accessor :name, :type, :db
+    attr_reader :id
+
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        sql = "INSERT INTO pokemon (name, type) VALUES (?, ?)"
+        db.execute(sql, name, type)
+    end
+
+    def self.find(id, db)
+        sql = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+        Pokemon.new(id: sql[0], name: sql[1], type: sql[2], db: db)
+    end
 end
